ab530ca0884e55af66be2feaeb587ff2357f73ed
PiperF312 <cytct01@gmail.com>
2025-11-25 20:29:53 -0500

    Delete project/2025-11-25-26 directory

--- project/2025-11-25-26/Bullet-ID-Bot-discord-main/Launcher.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM === Navigate to the folder where this batch file lives ===
-cd /d "%~dp0"
-
-REM === Run the bot script inside the Bot folder ===
-python Bot\bot.py
-
-REM === Keep window open after execution ===
-pause
